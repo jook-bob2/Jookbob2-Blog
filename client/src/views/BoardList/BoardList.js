8a7b2af91abdfd547163b8f00673a668dd7d22e6
ehrmaks <qwefk123@naver.com>
2020-06-18 22:37:38 +0900

    update page-count

--- client/src/views/BoardList/BoardList.js
+++ client/src/views/BoardList/BoardList.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
 import Paper from '@material-ui/core/Paper'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
 import TableRow from '@material-ui/core/TableRow'
 import TableCell from '@material-ui/core/TableCell'
 import TablePagination from '@material-ui/core/TablePagination'
-//import Pagination from '@material-ui/lab/Pagination';
 import { makeStyles } from '@material-ui/core/styles'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import { SearchInput } from '../../components';
@@ -254,11 +252,6 @@ const BoardList = props => {
       </Paper>
     </div>
   );
-};
-
-BoardList.propTypes = {
-  page: PropTypes.number,
-  rowsPerPage: PropTypes.number
 };
 
 export default BoardList;
