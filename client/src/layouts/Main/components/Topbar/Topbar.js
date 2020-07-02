f04c12d3eab666c0f683a33ce92e05656bbc7f78
ehrmaks <qwefk123@naver.com>
2020-07-02 23:36:29 +0900

    Revert "update"
    This reverts commit 7be990b8cf4b26fe618d86bf2ee627fa1af8073a.

--- client/src/layouts/Main/components/Topbar/Topbar.js
+++ client/src/layouts/Main/components/Topbar/Topbar.js
@@ -13,7 +13,6 @@ import {post} from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
 import { getSessioning } from '../../../../store/actions/index';
 
-
 const useStyles = makeStyles(theme => ({
     root: {
         boxShadow: 'none',
