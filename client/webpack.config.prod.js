a21d64546e465d8ff6892d75053b5f3c2701c48a
ehrmaks <qwefk123@naver.com>
2020-09-24 05:27:43 +0900

    update ssl

--- client/webpack.config.prod.js
+++ client/webpack.config.prod.js
@@ -6,7 +6,7 @@ module.exports = {
     mode: 'development',
     entry: ['@babel/polyfill', './src/index.js'],
     output: {
-        publicPath: 'https://www.jookbob2.com/',
+        publicPath: 'http://www.jookbob2.com/',
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.[hash].js'
     },
@@ -68,7 +68,7 @@ module.exports = {
     ],
     devServer: {
         //http2: true,
-        https: true,
+        //https: true,
         // key: fs.readFileSync('etc/gogetssl/www_jookbob2_com.key'),
         // cert: fs.readFileSync('etc/gogetssl/www_jookbob2_com.crt'),
         // ca: fs.readFileSync('etc/gogetssl/www_jookbob2_com.ca-bundle'),
