e981d964c344a77eb7602ef291054aae79bd2f98
ehrmaks <qwefk123@naver.com>
2020-09-24 08:00:37 +0900

    update key-alias

--- server/src/main/java/com/management/SpringbootApplication.java
+++ server/src/main/java/com/management/SpringbootApplication.java
@@ -4,11 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class SpringbootApplication extends SpringBootServletInitializer {
 	
-	private Logger log = LoggerFactory.getLogger(this.getClass());
+	//private Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
