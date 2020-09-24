8d6864d9915cda9003b34a7009d3d537f6a64f38
ehrmaks <qwefk123@naver.com>
2020-09-24 11:13:01 +0900

    test

--- client/webpack.config.prod.js
+++ client/webpack.config.prod.js
@@ -85,7 +85,7 @@ module.exports = {
         ],
         proxy: {
             '**': {
-                target: 'https://web',
+                target: 'https://web:8443',
                 bypass: function (req, res, proxyOptions) {
                     if (req.headers.accept.indexOf('html') !== -1) {
                         console.log('Skipping proxy for browser request.');
