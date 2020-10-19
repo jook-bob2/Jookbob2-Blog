d1d64f2c450280ea9d04e5d038375dce3a6a18f4
ehrmaks <qwefk123@naver.com>
2020-10-19 10:15:39 +0900

    update replyTitle

--- client/src/views/BoardList/components/Reply/Reply.js
+++ client/src/views/BoardList/components/Reply/Reply.js
@@ -69,7 +69,7 @@ const styles = makeStyles(theme => ({
         textAlign: 'center',
     },
     replyTitle: {
-      paddingBottom: 10,
+      padding: '1em',
       borderBottom: '1px solid rgba(224, 224, 224, 1)'
     }
 }));
